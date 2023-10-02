dcca807c6d94af63bb8002eff73cbac282f136de
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-02 11:03:47 -0600

    Add basics of table

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 }
 
