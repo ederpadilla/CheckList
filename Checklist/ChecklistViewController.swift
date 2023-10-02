296617fd58d988559b810414f30ebb5aa85e364d
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-02 11:42:55 -0600

    Addd dummies

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -31,17 +31,17 @@ class ChecklistViewController: UITableViewController {
         // Add the following code
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
+        if indexPath.row % 5 == 0 {
             label.text = "Walk the dog"
-        } else if indexPath.row == 1 {
+          } else if indexPath.row % 5 == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row == 2 {
+          } else if indexPath.row % 5 == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row == 3 {
+          } else if indexPath.row % 5 == 3 {
             label.text = "Soccer practice"
-        } else if indexPath.row == 4 {
+          } else if indexPath.row % 5 == 4 {
             label.text = "Eat ice cream"
-        }
+          }
         // End of new code block
         
         return cell
