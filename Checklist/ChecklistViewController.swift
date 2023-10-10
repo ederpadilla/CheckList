7055c635cdd59f20fc0b0800f56133ffbe853838
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-09 20:16:10 -0600

    Fox bug of selection

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -53,19 +53,65 @@ class ChecklistViewController: UITableViewController {
         } else if indexPath.row == 4 {
             label.text = row4text
         }
+        configureCheckmark(for: cell, at: indexPath)
         return cell
     }
     
+    func configureCheckmark(
+        for cell: UITableViewCell,
+        at indexPath: IndexPath
+    ) {
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
     // MARK: - Table View Delegate
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
+            var isChecked = false
+            
+            if indexPath.row == 0 {
+                row0checked.toggle()
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked.toggle()
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked.toggle()
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked.toggle()
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked.toggle()
+                isChecked = row4checked
+            }
+            if isChecked {
                 cell.accessoryType = .checkmark
             } else {
                 cell.accessoryType = .none
             }
         }
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
