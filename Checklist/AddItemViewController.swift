fe526c279fa5ab6e1a4a9d778d86f77b819cde80
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-11 09:08:09 -0600

    Init second vc

---
+++ Checklist/AddItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.swift
+//  Checklist
+//
+//  Created by Eder  Padilla on 11/10/23.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
