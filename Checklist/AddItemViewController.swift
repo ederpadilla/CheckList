3ea527f4062cfafd156289f27465fd4289c9a679
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-11 11:43:10 -0600

    Fix

--- Checklist/AddItemViewController.swift
+++ Checklist/AddItemViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class AddItemViewController: UITableViewController {
     
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    weak var delegate: AddItemViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -24,12 +25,13 @@ class AddItemViewController: UITableViewController {
     
     // MARK: - Actions
     @IBAction func cancel() {
-        navigationController?.popViewController(animated: true)
+        delegate?.addItemViewControllerDidCancel(self)
     }
     
     @IBAction func done() {
-        print("Contents of the text field: \(textField.text!)")
-        navigationController?.popViewController(animated: true)
+        var item = ChecklistItem()
+        item.text = textField.text!
+        delegate?.addItemViewController(self, didFinishAdding: item)
     }
     
     // MARK: - Table View Delegates
@@ -57,4 +59,13 @@ extension AddItemViewController: UITextFieldDelegate {
         return true
     }
     
+}
+
+protocol AddItemViewControllerDelegate: AnyObject {
+    func addItemViewControllerDidCancel(
+        _ controller: AddItemViewController)
+    func addItemViewController(
+        _ controller: AddItemViewController,
+        didFinishAdding item: ChecklistItem
+    )
 }
