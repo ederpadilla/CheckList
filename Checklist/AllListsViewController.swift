dd740a1f8228d9ecc8eb872941beac4c1f88e82d
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-13 10:06:55 -0600

    Add new vc

---
+++ Checklist/AllListsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AllListsViewController.swift
+//  Checklist
+//
+//  Created by Eder  Padilla on 13/10/23.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
