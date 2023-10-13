0c8ec23e05656d90b99396774a49ee10f8079bca
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-13 10:17:20 -0600

    init and remove xib

--- Checklist/AllListsViewController.swift
+++ Checklist/AllListsViewController.swift
@@ -8,8 +8,21 @@
 import UIKit
 
 class AllListsViewController: UITableViewController {
-        
+    private let cellIdentifier = "ChecklistCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
     }
 }
