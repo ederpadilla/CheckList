32368f1a7437fb237136f54a1e140939d7508d6f
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-15 14:55:19 -0600

    Add saving

--- Checklist/AllListsViewController.swift
+++ Checklist/AllListsViewController.swift
@@ -117,4 +117,41 @@ extension AllListsViewController: ListDetailViewControllerDelegate {
         navigationController?.popViewController(animated: true)
     }
     
+    // MARK: - File saving
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(
+                to: dataFilePath(),
+                options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding list array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode(
+                    [Checklist].self,
+                    from: data)
+            } catch {
+                print("Error decoding list array: \(error.localizedDescription)")
+            }
+        }
+    }
 }
