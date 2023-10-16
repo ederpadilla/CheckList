958c50ba6a766288ab71c8156851a604fca9aa73
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-15 18:33:58 -0600

    Add new description

--- Checklist/viewcontrollers/AllListsViewController.swift
+++ Checklist/viewcontrollers/AllListsViewController.swift
@@ -67,7 +67,8 @@ class AllListsViewController: UITableViewController {
         let checklist = dataModel.lists[indexPath.row]
         cell.textLabel!.text = checklist.name
         cell.accessoryType = .detailDisclosureButton
-        cell.detailTextLabel!.text = "\(checklist.countUncheckedItems()) Remaining"
+        let count = checklist.countUncheckedItems()
+        cell.detailTextLabel!.text = count == 0 ? "All Done" : "\(count) Remaining"
         return cell
     }
     
