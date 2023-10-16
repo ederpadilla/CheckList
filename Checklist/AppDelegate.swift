2cd52afcb319b0d6f744123518595e525fb3a934
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-16 13:01:50 -0600

    REmove test notifications

--- Checklist/AppDelegate.swift
+++ Checklist/AppDelegate.swift
@@ -13,28 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) {granted, error in
-            if granted {
-                print("We have permission")
-            } else {
-                print("Permission denied")
-            }
-            
-            let content = UNMutableNotificationContent()
-            content.title = "Hello!"
-            content.body = "I am a local notification"
-            content.sound = UNNotificationSound.default
-            
-            let trigger = UNTimeIntervalNotificationTrigger(
-                timeInterval: 10,
-                repeats: false)
-            let request = UNNotificationRequest(
-                identifier: "MyNotification",
-                content: content,
-                trigger: trigger)
-            center.add(request)
-            center.delegate = self
-        }
+        center.delegate = self
         return true
     }
     
