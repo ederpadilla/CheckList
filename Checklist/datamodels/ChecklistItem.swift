6701e554998344d493880e936f7f99fe31c0700c
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-16 15:27:24 -0600

    Fix

--- Checklist/datamodels/ChecklistItem.swift
+++ Checklist/datamodels/ChecklistItem.swift
@@ -26,36 +26,43 @@ class ChecklistItem: NSObject, Codable {
         itemID = DataModel.nextChecklistItemID()
     }
     
+    deinit {
+        removeNotification()
+    }
+    
     func scheduleNotification() {
-        func scheduleNotification() {
-            if shouldRemind && dueDate > Date() {
-                
-                let content = UNMutableNotificationContent()
-                content.title = "Reminder:"
-                content.body = text
-                content.sound = UNNotificationSound.default
-                
-                
-                let calendar = Calendar(identifier: .gregorian)
-                let components = calendar.dateComponents(
-                    [.year, .month, .day, .hour, .minute],
-                    from: dueDate)
-                
-                let trigger = UNCalendarNotificationTrigger(
-                    dateMatching: components,
-                    repeats: false)
-                
-                let request = UNNotificationRequest(
-                    identifier: "\(itemID)",
-                    content: content,
-                    trigger: trigger)
-                
-                let center = UNUserNotificationCenter.current()
-                center.add(request)
-                
-                print("Scheduled: \(request) for itemID: \(itemID)")
-            }
+        removeNotification()
+        if shouldRemind && dueDate > Date() {
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Reminder:"
+            content.body = text
+            content.sound = UNNotificationSound.default
+            
+            
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents(
+                [.year, .month, .day, .hour, .minute],
+                from: dueDate)
+            
+            let trigger = UNCalendarNotificationTrigger(
+                dateMatching: components,
+                repeats: false)
+            
+            let request = UNNotificationRequest(
+                identifier: "\(itemID)",
+                content: content,
+                trigger: trigger)
+            
+            let center = UNUserNotificationCenter.current()
+            center.add(request)
+            
+            print("Scheduled: \(request) for itemID: \(itemID)")
         }
-        
+    }
+    
+    func removeNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: ["\(itemID)"])
     }
 }
